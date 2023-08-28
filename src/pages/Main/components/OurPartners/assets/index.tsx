f0e2817dc7aebfaf4db093d342ba447f6a77b1eb
gaev-art <forsakensr@gmail.com>
2023-08-28 13:47:20 +0200

    feat: add new partner

--- src/pages/Main/components/OurPartners/assets/index.tsx
+++ src/pages/Main/components/OurPartners/assets/index.tsx
@@ -1,7 +1,9 @@
 import Courtyard from './partnersLogos/Courtyard.png';
 import FriendlyRB from './partnersLogos/FriendlyRB.png';
 import goVilnius from './partnersLogos/goVilnius.svg';
+import helveticRuby from './partnersLogos/helvetic_ruby_logo.png';
 import rubycentral from './partnersLogos/rubycentral.png';
+import VilniusTechFusion from './partnersLogos/VilniusTechFusion-logo.svg';
 import VTPilnas from './partnersLogos/VTPilnas.png';
 
 export const partners = [
@@ -14,6 +16,11 @@ export const partners = [
     imageUrl: goVilnius,
     alt: 'Go Vilnius logo',
     link: 'https://www.govilnius.lt/'
+  },
+  {
+    imageUrl: VilniusTechFusion,
+    alt: 'Vilnius Tech Fusion logo',
+    link: 'https://vilniustechfusion.com/en/'
   },
   {
     imageUrl: VTPilnas,
@@ -29,5 +36,10 @@ export const partners = [
     imageUrl: rubycentral,
     alt: 'Ruby Central logo',
     link: 'https://rubycentral.org/'
+  },
+  {
+    imageUrl: helveticRuby,
+    alt: 'Helvetic Ruby logo',
+    link: 'https://helvetic-ruby.ch/'
   }
 ];
